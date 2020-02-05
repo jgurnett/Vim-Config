f35f7a578ec800dde647ad46d8792a6299481316
Joel Gurnett <gurnettt@mymacewan.ca>
2020-02-05 13:15:03 -0700

    removed useless commands from install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,3 @@ ln -s ~/.vim/vimrc ~/.vimrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c ":PlugInstall"
-vim -c ":q"
-vim -c ":q"
-echo "Pluggins Installed"
